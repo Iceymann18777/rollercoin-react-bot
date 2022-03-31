7fbf1276c15e8819c191264a5c3472289bc9740f
adam <redrum691@protonmail.com>
2022-03-31 07:19:20 -0700

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -14,4 +14,18 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+function sendToAnalytics(metric: any) {
+    const body = JSON.stringify(metric);
+    const url = 'https://rollercoin.com/analytics';
+
+    // Use `navigator.sendBeacon()` if available, falling back to `fetch()`
+    if (navigator.sendBeacon) {
+        navigator.sendBeacon(url, body);
+    } else {
+        fetch(url, {body, method: 'POST', keepalive: true}).then(function () {
+        });
+    }
+}
+
+reportWebVitals(sendToAnalytics);
